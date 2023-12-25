a5686bc81f2877205eb71fe3071ec68f385d4d32
magusd <vitor.lobs@gmail.com>
2023-12-25 07:05:51 -0300

    bouncing ball

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty
+from kivy.properties import ObjectProperty, StringProperty, NumericProperty, BooleanProperty, Clock
 
 from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Line)
 from kivy.graphics.context_instructions import Color
@@ -25,10 +25,42 @@ class CanvasExample4(Widget):
             Color(0,1,0,0.5, mode='rgba')
             Line(circle=(400,200,50), width=2)
             Line(rectangle=(700,500,150,100), width=5)
-            Rectangle(pos=(700,300), size=(150,100))
+            self.rect = Rectangle(pos=(1300,300), size=(150,100))
+    def on_button_a_click(self):
+        print('Button A Clicked')
+        x, y = self.rect.pos
+        w, h = self.rect.size
+        inc = dp(10)
+        diff = self.width - (x+w)
+        if diff < inc:
+            inc = diff
+        
+        self.rect.pos = (self.rect.pos[0]+inc, self.rect.pos[1])
 
 class CanvasExample5(Widget):
-    pass
+    vx = dp(3)
+    vy = dp(3)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        with self.canvas:
+            self.ball = Ellipse(pos=(100,100), size=(self.ball_size,self.ball_size))
+        
+        Clock.schedule_interval(self.update, 1/60)
+    
+    def on_size(self,w, h):
+        self.ball.pos = (self.center_x - self.ball_size/2, self.center_y - self.ball_size/2)
+    
+    def update(self, dt):
+        x, y = self.ball.pos
+        if x + self.ball_size > self.width or x < 0:
+            self.vx = self.vx * -1
+
+        if y + self.ball_size > self.height or y < 0:
+            self.vy = self.vy * -1
+        x += self.vx
+        y += self.vy
+        self.ball.pos = (x,y)
 
 class WidgetsExample(GridLayout):
     my_text = StringProperty("Hello")
